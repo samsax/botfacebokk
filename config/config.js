fe36b665c1a4e955be3ebf2c67dbb5515511847b
dieggger <dieggger@gmail.com>
2017-07-05 16:45:59 -0500

    Set port as 3000 in config

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT || 3000,
   LUIS: {
     APPID: process.env.LUIS_APP_ID || "replace_here",
     APPSECRET: process.env.LUIS_APP_SECRET || "replace_here"
