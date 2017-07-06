ebef812505b991196dc8c8bab7df79f784203a33
samsax <samuelsaxofon@gmail.com>
2017-07-05 21:11:36 -0500

    bot funcionando

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const LUISClient = require("luis-node-sdk");
 const config = require("./config/config");
-
+const Smooch = require('smooch-core');
 
 var app = express();
+
+// Smooch
+const smooch = new Smooch({
+    keyId: config.SMOOCH.KEYID,
+    secret: config.SMOOCH.SECRET,
+    scope: 'app'
+});
+
+// Luis
+let LUISclient = LUISClient({
+  appId: config.LUIS.APPID,
+  appKey: config.LUIS.APPSECRET,
+  verbose: true
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,8 +39,51 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.post('/message', (req, res) => {
-  res.end();
+app.post('/messages', (req, res) => {
+  // console.log(JSON.stringify(req.body, null, 4));
+
+  const appUserId = req.body.appUser._id;
+  const text = req.body.messages[0].text;
+
+  const name = req.body.messages[0].name;
+
+  console.log(text);
+
+  LUISclient.predict(text, {
+    //On success of prediction
+    onSuccess: function (response) {
+      console.log(response.topScoringIntent.intent);
+      switch (response.topScoringIntent.intent) {
+        case "greet":
+            smooch.appUsers.sendMessage(appUserId, {
+              type: 'text',
+              text: "Hola "+name+",bienvenido a FRIKYGAMES te daremos respuesta lo mas pronto posible",
+              role: 'appMaker'
+            }).then(response => {
+                res.end();
+            }).catch(err => {
+                console.log(err);
+                res.end();
+            });
+            break;
+          default:
+            smooch.appUsers.sendMessage(appUserId, {
+              type: 'text',
+              text: "Me gustaria ser humano para poder entenderte, intenta ser mas especifico",
+              role: 'appMaker'
+            }).then(response => {
+                res.end();
+            }).catch(err => {
+                res.end();
+            });
+      }
+    },
+    //On failure of prediction
+    onFailure: function (err) {
+      console.error(err);
+    }
+  });
+
 });
 
 // catch 404 and forward to error handler
--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
   LUIS: {
-    APPID: process.env.LUIS_APP_ID || "replace_here",
-    APPSECRET: process.env.LUIS_APP_SECRET || "replace_here"
+    APPID: process.env.LUIS_APP_ID || "43301da5-d61e-4c28-83e0-156d70d51a0f",
+    APPSECRET: process.env.LUIS_APP_SECRET || "064403832e5b4361a4ab3ac0a2beb95e"
   },
   SMOOCH: {
-    APPID: process.env.SMOOCH_APP_ID || "replace_here",
-    APPTOKEN: process.env.SMOOCH_APP_TOKEN || "replace_here",
-    KEYID: process.env.SMOOCH_KEY_ID || "replace_here",
-    SECRET: process.env.SMOOCH_SECRET || "replace_here"
+    APPID: process.env.SMOOCH_APP_ID || "595d79e43001a441013adc21",
+    APPTOKEN: process.env.SMOOCH_APP_TOKEN || "2gnh0zkgx9pqljups83o0s5ca",
+    KEYID: process.env.SMOOCH_KEY_ID || "app_595d807e7a40d32601eda565",
+    SECRET: process.env.SMOOCH_SECRET || "s0J5eLGBf0S-V81C6bV4QPdQ"
   }
 }
